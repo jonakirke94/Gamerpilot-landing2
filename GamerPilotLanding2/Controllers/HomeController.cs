625dc57572425c71c54b03e6cd1c625bc83bc26a
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-06-25 15:12:35 +0200

    finished header and added subscribtion

--- GamerPilotLanding2/Controllers/HomeController.cs
+++ GamerPilotLanding2/Controllers/HomeController.cs
@@ -5,28 +5,65 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GamerPilotLanding2.Models;
+using GamerPilotLanding2.Data;
 
 namespace GamerPilotLanding2.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        [HttpGet("forældre")]
+        public IActionResult Parents()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
-        public IActionResult Contact()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Subscribe(string email, string returnUrl)
         {
-            ViewData["Message"] = "Your contact page.";
+            if (string.IsNullOrEmpty(email))
+            {
+                TempData["Error"] = "Emailen kan ikke være tom";
+                return RedirectToAction(returnUrl);
+            }
 
-            return View();
+            if (_context.Subscriptions.Any(x => x.Email == email))
+            {
+                TempData["Success"] = "Det ser ud til, at du allerede er skrevet op!";
+                return RedirectToAction(returnUrl);
+            }
+
+            var user = new User()
+            {
+                Email = email,
+                Created = DateTime.Now
+            };
+
+            try
+            {
+                _context.Subscriptions.Add(user);
+                _context.SaveChanges();
+                TempData["Success"] = "Tillykke, du er nu på listen!";
+            }
+            catch (Exception)
+            {
+                TempData["Error"] = "Hov noget gik galt - Kontakt os gerne";
+            }
+
+            return RedirectToAction(returnUrl);
         }
 
         public IActionResult Error()
