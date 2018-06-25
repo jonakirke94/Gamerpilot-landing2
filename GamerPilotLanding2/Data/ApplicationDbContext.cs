d42c07f081bf09981a8919649b6837c6afc1d108
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-06-25 12:24:46 +0200

    set up base project

---
+++ GamerPilotLanding2/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using GamerPilotLanding2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamerPilotLanding2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Feedback>().ToTable("Feedback");
+            modelBuilder.Entity<User>().ToTable("Subscriptions");
+
+        }
+
+        public DbSet<Feedback> Feedback { get; set; }
+        public DbSet<User> Subscriptions { get; set; }
+
+    }
+}
---
+++ GamerPilotLanding2/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GamerPilotLanding2.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GamerPilotLanding2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddRouting(options => options.LowercaseUrls = true);
+
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+
+            context.Database.EnsureCreated();
+        }
+    }
+}
